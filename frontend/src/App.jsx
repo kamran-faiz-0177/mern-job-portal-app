58f1e24d47ff0547391121aafc71e12b55cf4e1b
kamran <muhammad.kamran.cs@gmail.com>
2025-09-04 23:20:22 +0500

    created the pages and components folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Navbar from './Components/Navbar'
+import Job from './Pages/Job'
 
 const App = () => {
   return (
-    <div className='bg-red-300'>App</div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="job" element={<Job />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='bg-black w-full h-[10vh]'>
+            <div className='w-[80%] mx-auto flex items-center justify-around py-5 border-b-white border-b-2 text-white'>
+                <div>
+                    <h1 className='text-3xl font-bold'>JOBLANE</h1>
+                </div>
+                <div className='flex gap-3'>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/job">Jobs</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </div>
+                <div className='flex gap-2'>
+                    <button className='bg-purple-600 py-1 px-3 rounded-md'>Login</button>
+                    <button className='bg-purple-600 py-1 px-3 rounded-md'>Register</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
